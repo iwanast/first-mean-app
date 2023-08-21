29e69f8ba317c003a2ea1c7214605762c164994a
iwanast <staedeli.iwana@gmail.com>
2023-08-21 09:51:18 +0200

    Angular form used for the form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Post } from '../post.model';
 
@@ -13,10 +14,13 @@ export class PostCreateComponent {
   /*Output is a decorator so Angular is aware that this is an event you can listen to this from the parent component this means in the component where you are using the selector */
   @Output() postCreated = new EventEmitter<Post>();
   newPost = '';
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
     this.postCreated.emit(post);
   }
